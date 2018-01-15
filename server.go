c71e020280e4c6841ce993a5472dc2c2baf925d8
Daniël W. Crompton <daniel.crompton@gmail.com>
2018-01-15 21:31:44 +0100

    init

---
+++ server.go
@@ -0,0 +1,38 @@
+// Initial code from https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+/* A Simple function to verify error */
+func CheckError(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+}
+
+func main() {
+	/* Lets prepare a address at any address at port 1700*/
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":1700")
+	CheckError(err)
+
+	/* Now listen at selected port */
+	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	CheckError(err)
+	defer ServerConn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := ServerConn.ReadFromUDP(buf)
+		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
+
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+	}
+}
