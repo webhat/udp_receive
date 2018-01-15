3f044bc9894998b4ebc79d1bf0376f8cc44f578a
Daniël W. Crompton <daniel.crompton@gmail.com>
2018-01-15 22:16:11 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+LoRa UDP Proxy
+==============
+
+Simple UDP proxy for debugging UDP Packet Forwarder LoRa messages
--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 // Initial code from https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/
+
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -16,6 +18,10 @@ func CheckError(err error) {
 }
 
 func main() {
+
+	host := flag.String("host", "", "hostname:port")
+	flag.Parse()
+
 	/* Lets prepare a address at any address at port 1700*/
 	ServerAddr, err := net.ResolveUDPAddr("udp", ":1700")
 	CheckError(err)
@@ -25,10 +31,16 @@ func main() {
 	CheckError(err)
 	defer ServerConn.Close()
 
+	conn, err := net.Dial("udp", *host)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+
 	buf := make([]byte, 1024)
 
 	for {
 		n, addr, err := ServerConn.ReadFromUDP(buf)
+		fmt.Fprintf(conn, string(buf[0:n]))
 		fmt.Println("Received ", string(buf[0:n]), " from ", addr)
 
 		if err != nil {
